67051a5610e69a9be853ea1b1d202f4a479ea8da
f8419 <stevenyy.tsai@evertrust.com.tw>
2015-12-03 17:11:50 +0800

    Demo 2

---
+++ Demo_EditTemplate/Controllers/DynamicController.cs
@@ -0,0 +1,47 @@
+﻿using Demo_EditTemplate.Models;
+using Demo_EditTemplate.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_EditTemplate.Controllers
+{
+    public class DynamicController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CategoryViewModel
+            {
+                id = Guid.NewGuid(),
+                name = "書籍",
+                productList = new List<Product>
+                {
+                    new Product
+                    {
+                        id = Guid.NewGuid(),
+                        name = "碧血劍"
+                    },
+                    new Product
+                    {
+                        id = Guid.NewGuid(),
+                        name = "魯蛇傳奇"
+                    },
+                    new Product
+                    {
+                        id = Guid.NewGuid(),
+                        name = "X戰警之庵是金鋼朗"
+                    }
+                }
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CategoryViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Demo_EditTemplate/HtmlHelper/MyHtmlHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+namespace Demo_EditTemplate.HtmlHelper
+{
+    public static class MyHtmlHelper
+    {
+        public static MvcHtmlString EditorForMany<TModel, TValue>(
+            this HtmlHelper<TModel> html,
+            Expression<Func<TModel, IEnumerable<TValue>>> expression,
+            string htmlFieldName = null) where TModel : class
+        {
+            var items = expression.Compile()(html.ViewData.Model);
+            var sb = new StringBuilder();
+            var hasPrefix = false;
+
+            if (String.IsNullOrEmpty(htmlFieldName))
+            {
+                var prefix = html.ViewContext.ViewData.TemplateInfo.HtmlFieldPrefix;
+                hasPrefix = !String.IsNullOrEmpty(prefix);
+                htmlFieldName = (prefix.Length > 0 ? (prefix + ".") : String.Empty) + ExpressionHelper.GetExpressionText(expression);
+            }
+
+            if (items != null)
+            {
+                foreach (var item in items)
+                {
+                    var dummy = new { Item = item };
+                    var guid = Guid.NewGuid().ToString();
+
+                    var memberExp = Expression.MakeMemberAccess(Expression.Constant(dummy), dummy.GetType().GetProperty("Item"));
+                    var singleItemExp = Expression.Lambda<Func<TModel, TValue>>(memberExp, expression.Parameters);
+
+                    sb.Append(String.Format(@"<input type=""hidden"" name=""{0}.Index"" value=""{1}"" />", htmlFieldName, guid));
+                    sb.Append(html.EditorFor(singleItemExp, null, String.Format("{0}[{1}]", hasPrefix ? ExpressionHelper.GetExpressionText(expression) : htmlFieldName, guid)));
+                }
+            }
+            return new MvcHtmlString(sb.ToString());
+        }
+    }
+}
